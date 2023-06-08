064a3eda2ce768d45a5279a63ce1bdd4f8addbac
zerozhe588@gmail.com <zerozhe588@gmil.com>
2023-06-08 14:34:11 +0800

    add 20230608001branch

--- buffered/ChangeFileExtension.java
+++ buffered/ChangeFileExtension.java
@@ -7,33 +7,34 @@ import java.io.IOException;
 public class ChangeFileExtension {
     public static void main(String[] args) throws IOException {
         // 副檔名替換 txt -> Xml;
-        // String path = "/Users/ben/Documents/buffered/buffer.txt";
-        // int index = path.lastIndexOf("/");
-        // String fileName = path.substring(index + 1);
-        // System.out.println(fileName);
-        // String newName = fileName.replaceAll(".txt", ".xml");
-        // System.out.println(newName);
+        String path = "Users/ben/Documents/transforFile/buffered/buffer.txt";
+        int index = path.lastIndexOf("/");
+        String fileName = path.substring(index + 1);
+        System.out.println(fileName);
+        String newName = fileName.replaceAll(".txt", ".xml");
+        System.out.println(newName);
 
-        String fileN = "/Users/ben/Documents/buffered";
-        String newfile = "/Users/ben/Documents/buffered/testdemo";
-        File file = new File(fileN);
-        File file1 = new File(newfile);
-        file1.mkdir();
-        // file1.delete();
-        String[] arr = file.list();
-        for (String i : arr) {
-            System.out.println(i);
-        }
+        // 建立資料夾 讀取檔案
+        // String fileN = "/Users/ben/Documents/transforFile/buffered";
+        // String newfile = "/Users/ben/Documents/transforFile/aaa";
+        // File file = new File(fileN);
+        // File file1 = new File(newfile);
+        // file1.mkdir();
+        // // file1.delete();
+        // String[] arr = file.list();
+        // for (String i : arr) {
+        // System.out.println(i);
+        // }
 
-        String fileName1 = "/Users/ben/Documents/buffered/buffer.txt";
-        read(fileName1);
+        // String fileName1 = "/Users/ben/Documents/transforFile/buffered/buffer.txt";
+        // read(fileName1);
     }
 
-    public static void read(String fileName1) throws IOException {
-        FileReader fr = new FileReader(fileName1);
-        BufferedReader br = new BufferedReader(fr);
-        while (br.ready()) {
-            System.out.println(br.readLine());
-        }
-    }
+    // public static void read(String fileName1) throws IOException {
+    // FileReader fr = new FileReader(fileName1);
+    // BufferedReader br = new BufferedReader(fr);
+    // while (br.ready()) {
+    // System.out.println(br.readLine());
+    // }
+    // }
 }
--- buffered/Replace.java
+++ buffered/Replace.java
@@ -18,6 +18,7 @@ public class Replace {
     static String oldWord = "";
     static String newWord = "";
 
+    // txtChangeWord 單存txt當案例面 全的apple 要換成 banana 或是 , 換成。
     public static void txtChangeWord(String file, String oldWord, String newWord) throws IOException {
         oldWord = ",";
         newWord = ">";
@@ -43,7 +44,8 @@ public class Replace {
     }
 
     /**
-     * @param infile         //文件
+     * @param infile         //輸入文件
+     * @param targetfile     //目標文件
      * @param LineNo         //指定謀行
      * @param LineToBeInsert //加入你要的文字內容
      * @throws IOException
@@ -58,15 +60,8 @@ public class Replace {
         PrintWriter out = new PrintWriter(fos);
         // 保存一行數據
         String thisLine;
-        String countLine;
         // 行號從1開始
         int i = 1;
-        // int max = 0;
-        // while ((countLine = br.readLine()) != null) {
-        // out.println(countLine);
-        // max++;
-        // }
-        // System.out.println(max);
 
         while ((thisLine = br.readLine()) != null) {
             if (i == LineNo) {
@@ -88,11 +83,46 @@ public class Replace {
 
     }
 
+    public void endInsertStrinf(File infile, int LineNo, String endString, File targetfile) throws IOException {
+        File outfile = File.createTempFile("name", ".tmp");
+        // 輸入
+        FileInputStream fis = new FileInputStream(infile);
+        BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+        // 輸出
+        FileOutputStream fos = new FileOutputStream(outfile);
+        PrintWriter out = new PrintWriter(fos);
+        // 保存一行數據
+        String thisLine;
+        int i = 1;
+        while ((thisLine = br.readLine()) != null) {
+            if (i == LineNo) {
+                out.println(endString);
+            }
+            out.println(thisLine);
+            i++;
+        }
+        System.out.println(i);
+        if ((i - 1) < LineNo) {
+            out.println(endString);
+        }
+        out.flush();
+        out.close();
+        br.close();
+
+        // infile.delete();
+        outfile.renameTo(targetfile);
+
+    }
+
     public static void main(String[] args) throws IOException {
         // txtChangeWord(file, oldWord, newWord);
         Replace rp = new Replace();
-        File infile = new File("/Users/ben/Documents/buffered/buffer.txt");
-        rp.insertStringInFile(infile, 19, "this is 6 line");
+        File infile = new File("/Users/ben/Documents/transforfile/buffered/buffer.txt");
+        File targetfile = new File("/Users/ben/Documents/transforfile/buffered/buffer.xml");
+        String LineToBeInsert = "<?xml version='1.0' encoding='UTF-8'?> <web-app>";
+        String endString = "</web-app>";
+        rp.insertStringInFile(infile, 1, LineToBeInsert);
+        rp.endInsertStrinf(infile, 1000000, endString, targetfile);
 
     }
 
