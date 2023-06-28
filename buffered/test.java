00d338eb72bfef5b44a22206e35866efa567959b
zerozhe588@gmail.com <zerozhe588@gmil.com>
2023-06-28 14:38:28 +0800

    add String test git branch aaa001

--- buffered/test.java
+++ buffered/test.java
@@ -1,5 +1,6 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("this is git restore test file");
+        System.out.println("add aaa001 git checkout -b aaa001");
     }
 }
